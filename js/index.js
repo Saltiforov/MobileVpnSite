745cbd9f5766e73c1ff51559d7dcf383050837e2
Михаил Виноградчий <saltiforvdeveloper@gmail.com>
2021-04-25 22:21:03 +0300

    init commit

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    // autoplay: {
+    //     delay: 3000,
+    // },
+
+});
+function scrollMenu(){
+    let scrollPos = 100
+    let navbar = document.querySelector('.navbar')
+    if (document.body.scrollTop > scrollPos || document.documentElement.scrollTop > scrollPos){
+        navbar.classList.add('active')
+    }else {
+        navbar.classList.remove('active')
+    }
+}
+window.onscroll =  () =>  {scrollMenu()}
+
+const burger = document.querySelector('.header__burger')
+const navbarMenu = document.querySelector('.navbar-nav')
+burger.addEventListener('click', () => {
+    if (burger.classList.contains('active')){
+        burger.classList.remove('active')
+        navbarMenu.classList.remove('navbar-active')
+    }else {
+        burger.classList.add('active')
+        navbarMenu.classList.add('navbar-active')
+    }
+})
+
